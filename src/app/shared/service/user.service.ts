c38527d2e4a2af1400461b7c354da1ade0a80e02
BuildTools <shyndard@gmail.com>
2020-06-05 20:18:29 +0200

    Add user personnal registration

---
+++ src/app/shared/service/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../entity/user';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private usersBaseUrl = 'https://footslime-api.o2c.shyndard.eu/users';
+
+  constructor(private http: HttpClient) {}
+
+  createUser(username: String): Observable<User> {
+    return this.http.post<User>(this.usersBaseUrl, { name: username });
+  }
+
+  getAll(): Observable<User[]> {
+    return this.http.get<User[]>(this.usersBaseUrl);
+  }
+}
