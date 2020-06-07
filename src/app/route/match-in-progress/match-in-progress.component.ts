5d0df179fd9392f46df45d2b2b925e3a5ad83196
Custo <shyndard@gmail.com>
2020-06-07 15:12:00 +0200

    :bug: Fix interval request not ended

--- src/app/route/match-in-progress/match-in-progress.component.ts
+++ src/app/route/match-in-progress/match-in-progress.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Match } from 'src/app/shared/entity/match';
 import { MatchService } from 'src/app/shared/service/match.service';
 import { interval } from 'rxjs';
@@ -8,18 +8,24 @@ import { interval } from 'rxjs';
   templateUrl: './match-in-progress.component.html',
   styleUrls: ['./match-in-progress.component.scss']
 })
-export class MatchInProgressComponent implements OnInit {
+export class MatchInProgressComponent implements OnInit, OnDestroy {
 
   matchs: Match[];
+  private actualization;
 
   constructor(private matchService: MatchService) {}
 
   ngOnInit(): void {
-    interval(2000).subscribe(x => {
+    this.actualization = interval(2000).subscribe(() => {
       this.loadMatchs();
     });
     this.loadMatchs();
   }
+
+  ngOnDestroy(): void {
+    this.actualization.unsubscribe()
+  }
+
 
   private loadMatchs() {
     this.matchService.getInProgress().subscribe((response: Match[]) => {
--- src/app/route/matchs/matchs.component.ts
+++ src/app/route/matchs/matchs.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Match } from 'src/app/shared/entity/match';
 import { MatchService } from 'src/app/shared/service/match.service';
 import { interval } from 'rxjs';
@@ -9,17 +9,22 @@ import { interval } from 'rxjs';
   templateUrl: './matchs.component.html',
   styleUrls: ['./matchs.component.scss']
 })
-export class MatchsComponent implements OnInit {
+export class MatchsComponent implements OnInit, OnDestroy {
 
   matchs: Match[];
+  private actualization;
 
   constructor(private matchService: MatchService) {}
 
   ngOnInit(): void {
-    interval(2000).subscribe(x => {
+    this.actualization = interval(2000).subscribe(() => {
       this.loadMatchs();
     });
     this.loadMatchs();
+  }
+
+  ngOnDestroy(): void {
+    this.actualization.unsubscribe()
   }
 
   private loadMatchs() {
